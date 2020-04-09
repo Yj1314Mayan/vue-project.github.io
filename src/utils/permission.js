13d58a5927f0c6d498839c3e4c78e4ac0a8f26e6
e <1961878857@qq.com>
2020-04-09 16:29:21 +0800

    添加登录页

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -1,15 +1,14 @@
-import router from '../router'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
+import router from "../router"
+import NProgress from "nprogress" // progress bar
+import "nprogress/nprogress.css" // progress bar style
 
 router.beforeEach((to, from, next) => {
-  console.log(to, from, next)
-  // start progress bar
-  NProgress.start()
-  next()
+	// start progress bar
+	NProgress.start()
+	next()
 })
 
 router.afterEach(() => {
-  // finish progress bar
-  NProgress.done()
+	// finish progress bar
+	NProgress.done()
 })
