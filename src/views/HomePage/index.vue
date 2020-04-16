53a6574e17c39ba7784760cac49adb814379acba
e <1961878857@qq.com>
2020-04-16 11:20:07 +0800

    修改

--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -1,15 +1,38 @@
 <template>
-	<div class="home-page">
-		<Echarts></Echarts>
-	</div>
+  <div class="home-page">
+    <Echarts :orgOptions="orgOptions"></Echarts>
+  </div>
 </template>
 
 <script>
 import Echarts from "../../compoments/echarts"
 export default {
-	components: {
-		Echarts,
-	},
+  components: {
+    Echarts,
+  },
+  data () {
+    return {
+      orgOptions: {},
+    }
+  },
+  mounted () {
+    this.orgOptions = {
+      xAxis: {
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: "line",
+          smooth: true,
+        },
+      ],
+    }
+  },
 }
 </script>
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,28 +1,30 @@
 <template>
-	<el-container>
-		<!-- 左侧侧边栏 -->
-		<el-aside :width="isCollapse ? '210px' : '65px'">
-			<elMenu ref="elMenu" :isCollapse="isCollapse"></elMenu>
-		</el-aside>
-		<!-- 内容区域 -->
-		<el-container>
-			<!-- 头部 -->
-			<el-header>
-				<Header ref="Header"></Header>
-			</el-header>
-			<!-- 主体 -->
-			<el-main>
-				<!-- 路由缓存 -->
-				<elTab></elTab>
-				<!-- 页面入口 -->
-				<div class="main">
-					<keep-alive>
-						<router-view></router-view>
-					</keep-alive>
-				</div>
-			</el-main>
-		</el-container>
-	</el-container>
+  <el-container>
+    <!-- 左侧侧边栏 -->
+    <el-aside :width="isCollapse ? '210px' : '65px'">
+      <elMenu ref="elMenu" :isCollapse="isCollapse"></elMenu>
+    </el-aside>
+    <!-- 内容区域 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>
+        <Header ref="Header"></Header>
+      </el-header>
+      <!-- 主体 -->
+      <el-main>
+        <!-- 路由缓存 -->
+        <elTab></elTab>
+        <!-- 页面入口 -->
+        <div class="main">
+          <transition name="slide-fade">
+            <keep-alive>
+              <router-view></router-view>
+            </keep-alive>
+          </transition>
+        </div>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -31,37 +33,48 @@ import Header from "@/views/Home/components/el-header.vue"
 import elTab from "@/views/Home/components/el-tab.vue"
 import { mapState } from "vuex"
 export default {
-	components: {
-		elMenu,
-		Header,
-		elTab,
-	},
-	computed: {
-		...mapState({
-			isCollapse: (state) => state.home.isCollapse,
-		}),
-	},
+  components: {
+    elMenu,
+    Header,
+    elTab,
+  },
+  computed: {
+    ...mapState({
+      isCollapse: (state) => state.home.isCollapse,
+    }),
+  },
 }
 </script>
 
 <style>
 .el-container {
-	width: 100%;
-	height: 100%;
+  width: 100%;
+  height: 100%;
 }
 .el-main {
-	padding: 40px 0 0 0;
-	background-color: #f0f2f5;
-	position: relative;
+  padding: 40px 0 0 0;
+  background-color: #f0f2f5;
+  position: relative;
 }
 .el-header {
-	height: 50px !important;
-	line-height: 50px;
-	padding: 0;
+  height: 50px !important;
+  line-height: 50px;
+  padding: 0;
 }
 .main {
-	background-color: #fff;
-	width: 100%;
-	height: 100%;
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
