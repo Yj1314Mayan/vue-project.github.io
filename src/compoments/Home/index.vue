b8c3732aa3a935db3ea53001071793b1a3da39ff
e <1961878857@qq.com>
2020-04-20 13:59:56 +0800

    修改间距

--- src/compoments/Home/index.vue
+++ src/compoments/Home/index.vue
@@ -1,156 +1,153 @@
 <template>
-	<div class="home-page">
-		<Echarts :orgOptions="orgOptions" :width="600" :height="600"></Echarts>
-		<Echarts
-			:orgOptions="orgOptionsData"
-			:width="600"
-			:height="600"
-		></Echarts>
-	</div>
+  <div class="home-page">
+    <Echarts :orgOptions="orgOptions" width="39%" height="600px"></Echarts>
+    <Echarts :orgOptions="orgOptionsData" width="60%" height="600px"></Echarts>
+  </div>
 </template>
 
 <script>
 import Echarts from "../../compoments/echarts"
 export default {
-	components: {
-		Echarts,
-	},
-	data() {
-		return {
-			orgOptions: {
-				title: {
-					text: "某站点用户访问来源",
-					subtext: "纯属虚构",
-					left: "center",
-				},
-				tooltip: {
-					trigger: "item",
-					formatter: "{a} <br/>{b} : {c} ({d}%)",
-				},
-				legend: {
-					orient: "vertical",
-					left: "left",
-					data: [
-						"直接访问",
-						"邮件营销",
-						"联盟广告",
-						"视频广告",
-						"搜索引擎",
-					],
-				},
-				series: [
-					{
-						name: "访问来源",
-						type: "pie",
-						radius: "55%",
-						center: ["50%", "60%"],
-						data: [
-							{ value: 335, name: "直接访问" },
-							{ value: 310, name: "邮件营销" },
-							{ value: 234, name: "联盟广告" },
-							{ value: 135, name: "视频广告" },
-							{ value: 1548, name: "搜索引擎" },
-						],
-						emphasis: {
-							itemStyle: {
-								shadowBlur: 10,
-								shadowOffsetX: 0,
-								shadowColor: "rgba(0, 0, 0, 0.5)",
-							},
-						},
-					},
-				],
-			},
-			orgOptionsData: {
-				title: {
-					text: "南丁格尔玫瑰图",
-					subtext: "纯属虚构",
-					left: "center",
-				},
-				tooltip: {
-					trigger: "item",
-					formatter: "{a} <br/>{b} : {c} ({d}%)",
-				},
-				legend: {
-					left: "center",
-					top: "bottom",
-					data: [
-						"rose1",
-						"rose2",
-						"rose3",
-						"rose4",
-						"rose5",
-						"rose6",
-						"rose7",
-						"rose8",
-					],
-				},
-				toolbox: {
-					show: true,
-					feature: {
-						mark: { show: true },
-						dataView: { show: true, readOnly: false },
-						magicType: {
-							show: true,
-							type: ["pie", "funnel"],
-						},
-						restore: { show: true },
-						saveAsImage: { show: true },
-					},
-				},
-				series: [
-					{
-						name: "半径模式",
-						type: "pie",
-						radius: [20, 110],
-						center: ["25%", "50%"],
-						roseType: "radius",
-						label: {
-							show: false,
-						},
-						emphasis: {
-							label: {
-								show: true,
-							},
-						},
-						data: [
-							{ value: 10, name: "rose1" },
-							{ value: 5, name: "rose2" },
-							{ value: 15, name: "rose3" },
-							{ value: 25, name: "rose4" },
-							{ value: 20, name: "rose5" },
-							{ value: 35, name: "rose6" },
-							{ value: 30, name: "rose7" },
-							{ value: 40, name: "rose8" },
-						],
-					},
-					{
-						name: "面积模式",
-						type: "pie",
-						radius: [30, 110],
-						center: ["75%", "50%"],
-						roseType: "area",
-						data: [
-							{ value: 10, name: "rose1" },
-							{ value: 5, name: "rose2" },
-							{ value: 15, name: "rose3" },
-							{ value: 25, name: "rose4" },
-							{ value: 20, name: "rose5" },
-							{ value: 35, name: "rose6" },
-							{ value: 30, name: "rose7" },
-							{ value: 40, name: "rose8" },
-						],
-					},
-				],
-			},
-			data: [],
-		}
-	},
+  components: {
+    Echarts,
+  },
+  data () {
+    return {
+      orgOptions: {
+        title: {
+          text: "某站点用户访问来源",
+          subtext: "纯属虚构",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: [
+            "直接访问",
+            "邮件营销",
+            "联盟广告",
+            "视频广告",
+            "搜索引擎",
+          ],
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 234, name: "联盟广告" },
+              { value: 135, name: "视频广告" },
+              { value: 1548, name: "搜索引擎" },
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      },
+      orgOptionsData: {
+        title: {
+          text: "南丁格尔玫瑰图",
+          subtext: "纯属虚构",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)",
+        },
+        legend: {
+          left: "center",
+          top: "bottom",
+          data: [
+            "rose1",
+            "rose2",
+            "rose3",
+            "rose4",
+            "rose5",
+            "rose6",
+            "rose7",
+            "rose8",
+          ],
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            magicType: {
+              show: true,
+              type: ["pie", "funnel"],
+            },
+            restore: { show: true },
+            saveAsImage: { show: true },
+          },
+        },
+        series: [
+          {
+            name: "半径模式",
+            type: "pie",
+            radius: [20, 110],
+            center: ["25%", "50%"],
+            roseType: "radius",
+            label: {
+              show: false,
+            },
+            emphasis: {
+              label: {
+                show: true,
+              },
+            },
+            data: [
+              { value: 10, name: "rose1" },
+              { value: 5, name: "rose2" },
+              { value: 15, name: "rose3" },
+              { value: 25, name: "rose4" },
+              { value: 20, name: "rose5" },
+              { value: 35, name: "rose6" },
+              { value: 30, name: "rose7" },
+              { value: 40, name: "rose8" },
+            ],
+          },
+          {
+            name: "面积模式",
+            type: "pie",
+            radius: [30, 110],
+            center: ["75%", "50%"],
+            roseType: "area",
+            data: [
+              { value: 10, name: "rose1" },
+              { value: 5, name: "rose2" },
+              { value: 15, name: "rose3" },
+              { value: 25, name: "rose4" },
+              { value: 20, name: "rose5" },
+              { value: 35, name: "rose6" },
+              { value: 30, name: "rose7" },
+              { value: 40, name: "rose8" },
+            ],
+          },
+        ],
+      },
+      data: [],
+    }
+  },
 }
 </script>
 
 <style lang="scss" scope>
 .home-page {
-	display: flex;
+  display: flex;
+  padding: 40px;
 }
 </style>
--- src/compoments/echarts/index.vue
+++ src/compoments/echarts/index.vue
@@ -1,29 +1,26 @@
 <template>
-	<div
-		ref="myChart"
-		:style="{
-			width: width ? width + 'px' : '300px',
-			height: height ? height + 'px' : '300px',
-		}"
-	></div>
+  <div ref="myChart" :style="{
+			width: width ? width  : '300px',
+			height: height ? height : '300px',
+		}"></div>
 </template>
 
 <script>
 import echarts from "echarts"
 export default {
-	props: ["orgOptions", "width", "height"],
-	name: "echarts",
-	mounted() {
-		this.drawLine()
-	},
-	methods: {
-		drawLine() {
-			// 基于准备好的dom，初始化echarts实例
-			let myChart = echarts.init(this.$refs.myChart)
-			// 绘制图表
-			myChart.setOption(this.orgOptions)
-		},
-	},
+  props: ["orgOptions", "width", "height"],
+  name: "echarts",
+  mounted () {
+    this.drawLine()
+  },
+  methods: {
+    drawLine () {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(this.$refs.myChart)
+      // 绘制图表
+      myChart.setOption(this.orgOptions)
+    },
+  },
 }
 </script>
 
