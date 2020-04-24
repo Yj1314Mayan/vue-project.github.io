7ded034bd56cb7d923ea9b738b4588ac54e67ed4
e <1961878857@qq.com>
2020-04-24 14:27:59 +0800

    标签滚动

--- src/views/home/components/el-tab.vue
+++ src/views/home/components/el-tab.vue
@@ -1,127 +1,162 @@
 <template>
-  <div class="home-tab">
-    <div class="home-tab-left">
-      <i class="el-icon-caret-left" v-ripple></i>
-    </div>
-    <div class="home-tab-content">
-      <div class="home-tab-content-tag">
-        <span :class="`${ menusAtive == 0 ? 'ative' : ''}`" @click="setAtive(0, 'home')">首页 </span>
-        <span :class="`${ (index + 1) == menusAtive ? 'ative' : ''}`" v-for="(item, index) in menus" :key="index" @click="setAtive((index + 1), item)">{{item}}
-          <i class="el-icon-close" @click.stop="removeMenu(item)" />
-        </span>
-      </div>
-    </div>
-    <div class="home-tab-right">
-      <i class="el-icon-caret-right" v-ripple></i>
-    </div>
-    <div class="home-tab-delete">
-      <el-dropdown size="small">
-        <span class="el-dropdown-link">
-          <i class="el-icon-circle-close"></i>
-        </span>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>关闭当前</el-dropdown-item>
-          <el-dropdown-item>关闭所有</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </div>
-  </div>
+	<div class="home-tab">
+		<div class="home-tab-content" ref="wrapper">
+			<div class="home-tab-content-tag" ref="cont">
+				<span
+					:class="`${menusAtive == 0 ? 'ative' : ''}`"
+					@click="setAtive(0, 'home')"
+					>首页
+				</span>
+				<span
+					:class="`${index + 1 == menusAtive ? 'ative' : ''}`"
+					v-for="(item, index) in menus"
+					:key="index"
+					@click="setAtive(index + 1, item)"
+					>{{ item }}
+					<i class="el-icon-close" @click.stop="removeMenu(item)" />
+				</span>
+			</div>
+		</div>
+		<div class="home-tab-delete">
+			<el-dropdown size="small">
+				<span class="el-dropdown-link">
+					<i class="el-icon-circle-close"></i>
+				</span>
+				<el-dropdown-menu slot="dropdown">
+					<el-dropdown-item>关闭当前</el-dropdown-item>
+					<el-dropdown-item>关闭所有</el-dropdown-item>
+				</el-dropdown-menu>
+			</el-dropdown>
+		</div>
+	</div>
 </template>
 
 <script>
+import BScroll from "better-scroll"
 import { mapState, mapMutations } from "vuex"
 export default {
-  computed: {
-    ...mapState({
-      menus: (state) => state.router.menus,
-      menusAtive: (state) => state.router.menusAtive,
-    }),
-  },
-  methods: {
-    ...mapMutations(['removeMenus', 'setmenusAtive']),
-    removeMenu (item) {
-      this.removeMenus(item)
-      if (this.menus.length < 2) {
-        this.$router.push("home")
-        this.setmenusAtive(0)
-      } else {
-        let path = this.menus[this.menus.length - 1]
-        this.setmenusAtive(this.menus.length)
-        this.$router.push(path)
-      }
-    },
-    // 切换显示时候使用
-    setAtive (val, path) {
-      this.setmenusAtive(val)
-      this.$router.push(path)
-    }
-  },
-  data () {
-    return {}
-  }
+	computed: {
+		...mapState({
+			menus: (state) => state.router.menus,
+			menusAtive: (state) => state.router.menusAtive,
+		}),
+	},
+	methods: {
+		...mapMutations(["removeMenus", "setmenusAtive"]),
+		removeMenu(item) {
+			this.removeMenus(item)
+			if (this.menus.length < 2) {
+				this.$router.push("home")
+				this.setmenusAtive(0)
+			} else {
+				let path = this.menus[this.menus.length - 1]
+				this.setmenusAtive(this.menus.length)
+				this.$router.push(path)
+			}
+		},
+		// 切换显示时候使用
+		setAtive(val, path) {
+			this.setmenusAtive(val)
+			this.$router.push(path)
+		},
+		verScroll() {
+			let width = this.menus.length + 1 * 180 // 动态计算出滚动区域的大小，前面已经说过了，产生滚动的原因是滚动区域宽度大于父盒子宽度
+			this.$refs.cont.style.width = width + "px" // 修改滚动区域的宽度
+			this.$nextTick(() => {
+				if (!this.scroll) {
+					this.scroll = new BScroll(this.$refs.wrapper, {
+						startX: 0, // 配置的详细信息请参考better-scroll的官方文档，这里不再赘述
+						click: true,
+						scrollX: true,
+						scrollY: false,
+						eventPassthrough: "vertical",
+					})
+				} else {
+					this.scroll.refresh() //如果dom结构发生改变调用该方法
+				}
+			})
+		},
+	},
+	data() {
+		return {}
+	},
+	mounted() {
+		this.$nextTick(() => {
+			let timer = setTimeout(() => {
+				// 其实我也不想写这个定时器的，这相当于又嵌套了一层$nextTick，但是不这么写会失败
+				if (timer) {
+					clearTimeout(timer)
+					this.verScroll()
+				}
+			}, 0)
+		})
+	},
 }
 </script>
 
 <style lang="scss" scope>
 .home-tab {
-  position: absolute;
-  left: 0;
-  top: 5px;
-  height: 30px;
-  width: 100%;
-  line-height: 30px;
-  background-color: #fff;
-  display: flex;
+	position: absolute;
+	left: 0;
+	top: 5px;
+	height: 30px;
+	width: 100%;
+	line-height: 30px;
+	background-color: #fff;
+	display: flex;
 
-  .home-tab-left i,
-  .home-tab-delete i,
-  .home-tab-right i {
-    width: 30px;
-    height: 30px;
-    line-height: 30px;
-    text-align: center;
-  }
-  .home-tab-content {
-    flex: 1;
-    background-color: aliceblue;
-    padding: 0 5px;
-    overflow: hidden;
+	.home-tab-left i,
+	.home-tab-delete i,
+	.home-tab-right i {
+		width: 30px;
+		height: 30px;
+		line-height: 30px;
+		text-align: center;
+	}
+	.home-tab-content {
+		flex: 1;
+		background-color: aliceblue;
+		padding: 0 5px;
+		overflow: hidden;
+		margin: 0 20px;
 
-    span {
-      display: inline-block;
-      padding: 0 22px 0 10px;
-      box-sizing: border-box;
-      height: 22px;
-      line-height: 22px;
-      font-size: 13px;
-      border: 1px solid #409eff;
-      border-radius: 4px;
-      background-color: #fff;
-      margin: 0 5px;
-      position: relative;
-      &:first-child {
-        padding: 0 10px;
-      }
-      i {
-        position: absolute;
-        right: 3px;
-        top: 3px;
-        padding: 1px;
-        line-height: 13px;
-        &:hover {
-          background-color: #409eff;
-          color: #fff;
-          border-radius: 50%;
-        }
-      }
-    }
-    .ative {
-      background-color: #409eff;
-      color: #fff;
-    }
-  }
-  .home-tab-delete i {
-    border-left: 1px solid #ccc;
-  }
+		span {
+			display: inline-block;
+			padding: 0 22px 0 10px;
+			box-sizing: border-box;
+			height: 22px;
+			line-height: 22px;
+			font-size: 13px;
+			border: 1px solid #409eff;
+			border-radius: 4px;
+			background-color: #fff;
+			margin: 0 5px;
+			position: relative;
+			cursor: pointer;
+
+			&:first-child {
+				padding: 0 10px;
+			}
+			i {
+				position: absolute;
+				right: 3px;
+				top: 3px;
+				padding: 1px;
+				line-height: 13px;
+				&:hover {
+					background-color: #409eff;
+					color: #fff;
+					border-radius: 50%;
+				}
+			}
+		}
+		.ative {
+			background-color: #409eff;
+			color: #fff;
+		}
+	}
+	.home-tab-delete i {
+		border-left: 1px solid #ccc;
+	}
 }
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,30 +1,30 @@
 <template>
-  <el-container>
-    <!-- 左侧侧边栏 -->
-    <el-aside :width="isCollapse ? '210px' : '65px'">
-      <elMenu ref="elMenu" :isCollapse="isCollapse"></elMenu>
-    </el-aside>
-    <!-- 内容区域 -->
-    <el-container>
-      <!-- 头部 -->
-      <el-header>
-        <Header ref="Header"></Header>
-      </el-header>
-      <!-- 主体 -->
-      <el-main>
-        <!-- 路由缓存 -->
-        <elTab></elTab>
-        <!-- 页面入口 -->
-        <div class="main">
-          <transition name="fade-transform" mode="out-in">
-            <keep-alive>
-              <router-view></router-view>
-            </keep-alive>
-          </transition>
-        </div>
-      </el-main>
-    </el-container>
-  </el-container>
+	<el-container>
+		<!-- 左侧侧边栏 -->
+		<el-aside :width="isCollapse ? '210px' : '65px'">
+			<elMenu ref="elMenu" :isCollapse="isCollapse"></elMenu>
+		</el-aside>
+		<!-- 内容区域 -->
+		<el-container>
+			<!-- 头部 -->
+			<el-header>
+				<Header ref="Header"></Header>
+			</el-header>
+			<!-- 主体 -->
+			<el-main>
+				<!-- 路由缓存 -->
+				<elTab></elTab>
+				<!-- 页面入口 -->
+				<div class="main">
+					<transition name="fade-transform" mode="out-in">
+						<keep-alive>
+							<router-view></router-view>
+						</keep-alive>
+					</transition>
+				</div>
+			</el-main>
+		</el-container>
+	</el-container>
 </template>
 
 <script>
@@ -33,52 +33,53 @@ import Header from "@/views/Home/components/el-header.vue"
 import elTab from "@/views/Home/components/el-tab.vue"
 import { mapState } from "vuex"
 export default {
-  components: {
-    elMenu,
-    Header,
-    elTab,
-  },
-  computed: {
-    ...mapState({
-      isCollapse: (state) => state.home.isCollapse,
-    }),
-  },
+	components: {
+		elMenu,
+		Header,
+		elTab,
+	},
+	computed: {
+		...mapState({
+			isCollapse: (state) => state.home.isCollapse,
+		}),
+	},
 }
 </script>
 
 <style scope>
 .el-container {
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 }
 .el-main {
-  padding: 40px 0 0 0;
-  background-color: #f0f2f5;
-  position: relative;
+	padding: 40px 0 0 0;
+	background-color: #f0f2f5;
+	position: relative;
 }
 .el-header {
-  height: 50px !important;
-  line-height: 50px;
-  padding: 0;
+	height: 50px !important;
+	line-height: 50px;
+	padding: 0;
 }
 .main {
-  background-color: #fff;
-  width: 100%;
-  height: 100%;
-  padding: 40px;
+	background-color: #fff;
+	width: 100%;
+	height: 100%;
+	padding: 40px;
+	box-sizing: border-box;
 }
 .fade-transform-leave-active,
 .fade-transform-enter-active {
-  transition: all 0.5s;
+	transition: all 0.5s;
 }
 
 .fade-transform-enter {
-  opacity: 0;
-  transform: translateX(-30px);
+	opacity: 0;
+	transform: translateX(-30px);
 }
 
 .fade-transform-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
+	opacity: 0;
+	transform: translateX(30px);
 }
 </style>
