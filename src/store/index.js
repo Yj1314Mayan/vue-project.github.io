ed9d32dde27f5bb48be5b882126a5a41e906f52d
e <1961878857@qq.com>
2020-04-26 14:50:02 +0800

    添加vuex数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,18 @@ import Vue from "vue"
 import Vuex from "vuex"
 import home from "./models/home"
 import router from "./models/router"
+import VuexPersistence from "vuex-persist"
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+	storage: window.localStorage,
+})
 
 export default new Vuex.Store({
 	modules: {
 		home,
 		router,
 	},
+	plugins: [vuexLocal.plugin],
 })
