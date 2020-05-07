95e5debcfb3ad2b51a80b38222ce1a13dc691332
e <1961878857@qq.com>
2020-05-07 09:53:15 +0800

    添加

--- src/views/Table/index.vue
+++ src/views/Table/index.vue
@@ -10,7 +10,7 @@
       </el-table-column>
       <el-table-column label="操作" width="120">
         <template slot-scope="scope">
-          <el-button type="primary" size="mini">
+          <el-button type="primary" size="mini" @click="removeItem(scope.row.id)">
             移除
           </el-button>
         </template>
@@ -25,18 +25,22 @@ export default {
   data () {
     return {
       tableData: [{
+        id: 0,
         date: '2016-05-02',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄',
       }, {
+        id: 1,
         date: '2016-05-04',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄'
       }, {
+        id: 2,
         date: '2016-05-01',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄',
       }, {
+        id: 3,
         date: '2016-05-03',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄'
@@ -54,9 +58,16 @@ export default {
     },
     add () {
       this.tableData.push({
+        id: this.tableData.length && this.tableData[this.tableData.length - 1].id + 1 || 0,
         date: '2016-05-02',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄',
+      })
+    },
+    removeItem (id) {
+      this.tableData.splice(id, 1)
+      this.tableData.forEach((item, index) => {
+        item.id = index
       })
     }
   },
